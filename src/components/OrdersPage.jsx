6a4bc7cf76bca3d9028ee4cd9c4f25e8e050f070
Jmmichael19 <167400971+Jmmichael19@users.noreply.github.com>
2025-12-04 10:48:58 +0800

    Update OrdersPage.jsx

--- src/components/OrdersPage.jsx
+++ src/components/OrdersPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../services/supabase";
+import  supabase  from "../services/supabase";
 import { Clock, Receipt, Utensils, CheckCircle2, XCircle, Loader2 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
